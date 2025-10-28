efd60da5979c934b5eefa8188939c9cdbc196137
Ujjwalpdy <ujjwalp4238e@gmail.com>
2025-10-28 13:33:02 +0530

    Add files via upload

---
+++ package assignment/project 1/college/student/student.java
@@ -0,0 +1,8 @@
+package college.student;
+
+public class student {
+    public void showstudent() {
+        System.out.println("Student Name: Amol");
+        System.out.println("Roll No: 101");
+    }
+}
---
+++ package assignment/project 1/mainApp.java
@@ -0,0 +1,13 @@
+
+import college.student.student;
+import college.faculty.Faculty;
+
+public class mainApp {
+    public static void main(String[] args) {
+        student s = new student();
+        Faculty f = new Faculty();
+        s.showstudent();
+        f.showFaculty();
+    }
+}
+
